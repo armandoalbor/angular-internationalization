945a8c3a30ec8698c315f438302766725684f832
armando.albor <armando.albor@pricetravel.com>
2019-03-12 11:03:05 -0500

    Feature: Integra soporte para internacionalización China (zh)

--- multilingual-app/src/app/app.component.ts
+++ multilingual-app/src/app/app.component.ts
@@ -1,32 +1,22 @@
 import { Component } from '@angular/core';
+
+const NO_ALLOTMENT = "NoAllotment";
+const MAX_CAPACITY = "Max Capacity";
+const MAX_ADULTS = "Max Adults Capacity";
+const MAX_KIDS = "Max Kids Capacity";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  role = 'admin';
-  usersCount = 2;
-  phone = "01 (800) 570-0988";
+  phone: string;
+  message: string;
+  noAllotment: boolean;
 
-  NO_ALLOTMENT = "NoAllotment";
-  MAX_CAPACITY = "Max Capacity";
-  MAX_ADULTS = "Max Adults Capacity";
-  MAX_KIDS = "Max Kids Capacity";
-
-  message = "Max Capacity";
-
-  users = [
-    {
-      gender: 'male',
-      name: 'Armando Albor',
-      message: 'NoAllotment'
-    },
-    {
-      gender: 'female',
-      name: 'Anais Hernandez',
-      message: 'Max Capacity'
-    },
-  ]
+  constructor(){
+    this.phone = "01 (800) 570-0988";
+    this.message = NO_ALLOTMENT;
+    this.noAllotment = this.message === NO_ALLOTMENT;
+  }
 }
