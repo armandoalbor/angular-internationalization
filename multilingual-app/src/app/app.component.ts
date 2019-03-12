1d89ee1b52293fd18a7c05568d9dcae8bf91d719
armando.albor <armando.albor@pricetravel.com>
2019-03-11 19:05:05 -0500

    Feature: Angular i18n with demo for Price Travel Holding

--- multilingual-app/src/app/app.component.ts
+++ multilingual-app/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  role = 'admin';
+  usersCount = 2;
+  phone = "01 (800) 570-0988";
+
+  NO_ALLOTMENT = "NoAllotment";
+  MAX_CAPACITY = "Max Capacity";
+  MAX_ADULTS = "Max Adults Capacity";
+  MAX_KIDS = "Max Kids Capacity";
+
+  message = "Max Capacity";
+
+  users = [
+    {
+      gender: 'male',
+      name: 'Armando Albor',
+      message: 'NoAllotment'
+    },
+    {
+      gender: 'female',
+      name: 'Anais Hernandez',
+      message: 'Max Capacity'
+    },
+  ]
 }
